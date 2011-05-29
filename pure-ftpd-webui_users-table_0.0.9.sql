ee9b8ccbfac3dbe83ec462fade1f837d79e80ddf
MaZaY <mazay@mazay.home.dyndns.org>
2011-05-29 20:26:45 +0600

    MySQL table files added

--- pure-ftpd-webui_users-table_0.0.9.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `userlist` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
-  `user` varchar(50) COLLATE utf8_bin NOT NULL,
-  `pass` varchar(50) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
