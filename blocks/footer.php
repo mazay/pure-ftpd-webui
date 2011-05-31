f587f8e26f643bc28dd8ebfb73baac44b2b7f68a
MaZaY <mazay@mazay.home.dyndns.org>
2011-05-31 10:45:10 +0600

    Feddback link changed

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,2 @@
-<td class="bottom_border"><div align="left"><p class="footer">feedback: <a href="mailto:z.mazay@gmail.com?subject=Pure-FTPd web UI" title="z.mazay@gmail.com">z.mazay@gmail.com</a></p></div></td>
+<td class="bottom_border"><div align="left"><p class="footer">feedback: <a href="http://mazay.home.dyndns.org/redmine/projects/pure-ftpd-webui/boards">Pure-FTPd WebUI Forums</a></p></div></td>
 </tr>
