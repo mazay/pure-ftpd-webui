2649c50b0c2873f9ad88df35e1e81b63d9b5b2f8
MaZaY <mazay@mazay.home.dyndns.org>
2011-05-31 11:07:26 +0600

    Feedback link changed

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,2 @@
-<td class="bottom_border"><div align="left"><p class="footer">feedback: <a href="http://mazay.home.dyndns.org/redmine/projects/pure-ftpd-webui/boards">Pure-FTPd WebUI Forums</a></p></div></td>
+<td class="bottom_border"><div align="left"><p class="footer">feedback: <a href="http://mazay.home.dyndns.org/redmine/projects/pure-ftpd-webui/boards">Pure-FTPd WebUI</a></p></div></td>
 </tr>
