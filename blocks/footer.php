c7419d5873706cbd3878f0e39371a76f7586425d
MaZaY <mazay@mazay.home.dyndns.org>
2011-06-06 17:50:05 +0600

    beta 0.1.1

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,2 @@
-<td class="bottom_border"><div align="center"><p class="footer"><a href="http://mazay.home.dyndns.org/redmine/projects/pure-ftpd-webui">Pure-FTPd beta</a> 0.1.0 © 2010-2011 Yevgeniy Valeyev</p></div></td>
+<td class="bottom_border"><div align="center"><p class="footer"><a href="http://mazay.home.dyndns.org/redmine/projects/pure-ftpd-webui">Pure-FTPd beta</a> 0.1.1 © 2010-2011 Yevgeniy Valeyev</p></div></td>
 </tr>
