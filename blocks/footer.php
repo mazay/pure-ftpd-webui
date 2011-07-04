17667754ab2134d699d631728eaec637dc390a97
MaZaY <mazay@mazay.home.dyndns.org>
2011-07-04 23:03:28 +0600

    Fixed mistake in home page link

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,2 @@
-<td class="bottom_border"><div align="center"><p class="footer"><a href="http://pure-ftpd-webui.org/">Pure-FTPd beta</a> 0.1.1 © 2010-2011 Yevgeniy Valeyev</p></div></td>
+<td class="bottom_border"><div align="center"><p class="footer"><a href="http://pure-ftpd-webui.org/">Pure-FTPd WebUI</a> beta 0.1.1 © 2010-2011 Yevgeniy Valeyev</p></div></td>
 </tr>
