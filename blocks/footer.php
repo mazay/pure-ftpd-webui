f9320f25610ef8b1dc4f7b19ba32dcc0e629f520
MaZaY <mazay@mazay.home.dyndns.org>
2011-07-04 13:00:08 +0600

    Home page link changed

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,2 +1,2 @@
-<td class="bottom_border"><div align="center"><p class="footer"><a href="http://pure-ftpd-webui.org/projects/pure-ftpd-webui">Pure-FTPd beta</a> 0.1.1 © 2010-2011 Yevgeniy Valeyev</p></div></td>
+<td class="bottom_border"><div align="center"><p class="footer"><a href="http://pure-ftpd-webui.org/">Pure-FTPd beta</a> 0.1.1 © 2010-2011 Yevgeniy Valeyev</p></div></td>
 </tr>
